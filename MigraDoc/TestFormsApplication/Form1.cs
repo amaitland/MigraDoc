6af8210507ea37ef221c1943dfca46b6b5447545
amaitland <alex@maitlandcomputing.com.au>
2014-05-15 13:27:31 +1000

    Remove more traces of Rtf

--- MigraDoc/TestFormsApplication/Form1.cs
+++ MigraDoc/TestFormsApplication/Form1.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Drawing;
-using System.Text;
 using System.Windows.Forms;
 using MigraDoc.DocumentObjectModel;
 using MigraDoc.DocumentObjectModel.Shapes.Charts;
@@ -10,97 +6,86 @@ using MigraDoc.Rendering;
 
 namespace TestFormsApplication
 {
-    public partial class Form1 : Form
-    {
-        public Form1()
-        {
-            InitializeComponent();
-        }
+	public partial class Form1 : Form
+	{
+		public Form1()
+		{
+			InitializeComponent();
+		}
 
-        private void RenderRtf(object sender, EventArgs e)
-        {
-            var doc = CreateHelloWorld();
-            OpenRtf(doc);
-        }
+		private void RenderPdf(object sender, EventArgs e)
+		{
+			var doc = CreateHelloWorld();
+			OpenPdf(doc);
+		}
 
-        private void RenderPdf(object sender, EventArgs e)
-        {
-            var doc = CreateHelloWorld();
-            OpenPdf(doc);
-        }
+		private static Document CreateHelloWorld()
+		{
+			var doc = new Document();
+			var section = doc.AddSection();
+			var p = section.AddParagraph("Hello World");
+			p = section.AddParagraph();
+			p.AddImage(System.IO.Path.Combine(Environment.CurrentDirectory, "Diagram.png"));
 
-        private static Document CreateHelloWorld()
-        {
-            var doc = new Document();
-            var section = doc.AddSection();
-            var p = section.AddParagraph("Hello World");
-            p = section.AddParagraph();
-            p.AddImage(System.IO.Path.Combine(Environment.CurrentDirectory, "Diagram.png"));
+			DefineCharts(doc);
 
-            DefineCharts(doc);
+			return doc;
+		}
 
-            return doc;
-        }
+		public static void DefineCharts(Document document)
+		{
+			var paragraph = document.LastSection.AddParagraph("Chart Overview", "Heading1");
+			paragraph.AddBookmark("Charts");
+			document.LastSection.AddParagraph("Sample Chart", "Heading2");
+			var chart = new Chart();
 
-        public static void DefineCharts(Document document)
-        {
-            var paragraph = document.LastSection.AddParagraph("Chart Overview", "Heading1");
-            paragraph.AddBookmark("Charts");
-            document.LastSection.AddParagraph("Sample Chart", "Heading2");
-            var chart = new Chart();
+			chart.Left = 0;
+			chart.Width = Unit.FromCentimeter(15);
+			chart.Height = Unit.FromCentimeter(12);
+			var series = chart.SeriesCollection.AddSeries();
+			series.ChartType = ChartType.Column2D;
+			series.Add(new double[] { 1, 17, 45, 5, 3, 20, 11, 23, 8, 19 });
+			series.HasDataLabel = true;
 
-            chart.Left = 0;
-            chart.Width = Unit.FromCentimeter(15);
-            chart.Height = Unit.FromCentimeter(12);
-            var series = chart.SeriesCollection.AddSeries();
-            series.ChartType = ChartType.Column2D;
-            series.Add(new double[] { 1, 17, 45, 5, 3, 20, 11, 23, 8, 19 });
-            series.HasDataLabel = true;
+			series = chart.SeriesCollection.AddSeries();
+			series.ChartType = ChartType.Line;
+			series.Add(new double[] { 41, 7, 5, 45, 13, 10, 21, 13, 18, 9 });
 
-            series = chart.SeriesCollection.AddSeries();
-            series.ChartType = ChartType.Line;
-            series.Add(new double[] { 41, 7, 5, 45, 13, 10, 21, 13, 18, 9 });
+			var xseries = chart.XValues.AddXSeries();
+			xseries.Add("A", "B", "C", "D", "E", "F", "G", "H", "I", "J");
 
-            var xseries = chart.XValues.AddXSeries();
-            xseries.Add("A", "B", "C", "D", "E", "F", "G", "H", "I", "J");
+			chart.XAxis.MajorTickMark = TickMarkType.Outside;
+			chart.XAxis.Title.Caption = "X-Axis";
 
-            chart.XAxis.MajorTickMark = TickMarkType.Outside;
-            chart.XAxis.Title.Caption = "X-Axis";
+			chart.YAxis.MajorTickMark = TickMarkType.Outside;
+			chart.YAxis.HasMajorGridlines = true;
 
-            chart.YAxis.MajorTickMark = TickMarkType.Outside;
-            chart.YAxis.HasMajorGridlines = true;
+			chart.PlotArea.LineFormat.Color = Colors.DarkGray;
+			chart.PlotArea.LineFormat.Width = 1;
 
-            chart.PlotArea.LineFormat.Color = MigraDoc.DocumentObjectModel.Colors.DarkGray;
-            chart.PlotArea.LineFormat.Width = 1;
-
-            document.LastSection.Add(chart);
-        }
+			document.LastSection.Add(chart);
+		}
 
 
-        private static void OpenPdf(Document doc)
-        {
-            var filename = "c:\\temp\\test.pdf";
-            PdfDocumentRenderer pdf = new PdfDocumentRenderer(true, PdfSharp.Pdf.PdfFontEmbedding.None);
-            pdf.Document = doc;
-            pdf.RenderDocument();
-            pdf.Save(filename);
-            ShellExecute(filename, "");
-        }
+		private static void OpenPdf(Document doc)
+		{
+			var filename = "c:\\temp\\test.pdf";
+			PdfDocumentRenderer pdf = new PdfDocumentRenderer(true, PdfSharp.Pdf.PdfFontEmbedding.None);
+			pdf.Document = doc;
+			pdf.RenderDocument();
+			pdf.Save(filename);
+			ShellExecute(filename, "");
+		}
 
-        private static void OpenRtf(Document doc)
-        {
-            throw new NotImplementedException();
-        }
+		public static void ShellExecute(string file, string verb)
+		{
+			if (file == null) throw new ArgumentNullException("file");
 
-        public static void ShellExecute(string file, string verb)
-        {
-            if (file == null) throw new ArgumentNullException("file");
-
-            System.Diagnostics.ProcessStartInfo si = new System.Diagnostics.ProcessStartInfo();
-            si.UseShellExecute = true;
-            si.FileName = file;
-            si.Verb = verb;
-            System.Diagnostics.Process.Start(si);
-        }
-    }
+			System.Diagnostics.ProcessStartInfo si = new System.Diagnostics.ProcessStartInfo();
+			si.UseShellExecute = true;
+			si.FileName = file;
+			si.Verb = verb;
+			System.Diagnostics.Process.Start(si);
+		}
+	}
 }
